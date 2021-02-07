0dfa9d01f0ba51f4121723e903aeee34461d818d
Denis Stepanov <dondiego4697@mail.ru>
2021-02-07 22:51:08 +0300

    Удаляем куку при невалидном csrf

--- server/app/middleware/csrf.ts
+++ server/app/middleware/csrf.ts
@@ -4,14 +4,16 @@ import {wrap} from 'async-middleware';
 import {CSRF} from 'app/lib/csrf';
 import {config} from 'app/config';
 
-export const csrf = wrap<Request, Response>(async (req, _res, next) => {
+export const csrf = wrap<Request, Response>(async (req, res, next) => {
     if (!config['csrf.enable']) {
         return next();
     }
 
     if (CSRF.isTokenValid(req)) {
+        res.cookie('csrf_token', CSRF.generateToken(), {maxAge: config['csrf.token.ttl']});
         return next();
     }
 
+    res.clearCookie('csrf_token');
     throw Boom.forbidden('invalid csrf');
 });
